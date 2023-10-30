c3ff87526297230d69e2a9a22300614d738964dd
VinodNimbalkar <omvinod@yahoo.com>
2023-10-30 23:31:29 +0530

    :tada: Initial commit

---
+++ src/handlers.rs
@@ -0,0 +1,104 @@
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use chrono::{DateTime, Utc};
+use mongodb::{bson::doc, bson::oid::ObjectId, error, Client, Collection, Database};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Quote {
+    book: String,
+    quote: String,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+}
+
+impl Quote {
+    fn new(book: String, quote: String) -> Self {
+        let now = Utc::now();
+        Self {
+            book,
+            quote,
+            created_at: now,
+            updated_at: now,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateQuote {
+    book: String,
+    quote: String,
+}
+
+pub async fn health() -> StatusCode {
+    StatusCode::OK
+}
+
+pub async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "nothing to see here")
+}
+
+pub async fn create_quote(
+    State(client): State<Client>,
+    Json(payload): Json<CreateQuote>,
+) -> Result<(StatusCode, Json<Quote>), StatusCode> {
+    let quote = Quote::new(payload.book, payload.quote);
+    let collection = client.database("vinod").collection::<Quote>("quotes");
+    let _ = collection.insert_one(&quote, None).await;
+
+    Ok((StatusCode::CREATED, axum::Json(quote)))
+}
+
+pub async fn read_quotes(State(client): State<Client>) -> Result<Json<Vec<Quote>>, StatusCode> {
+    let collection = client.database("vinod").collection::<Quote>("quotes");
+
+    let mut quote_cursor = collection
+        .find(None, None)
+        .await
+        .expect("could not load quotes data.");
+    let mut quotes = Vec::new();
+
+    while quote_cursor.advance().await.unwrap() {
+        quotes.push(quote_cursor.deserialize_current().unwrap());
+    }
+
+    Ok(axum::Json(quotes))
+}
+
+pub async fn update_quote(
+    State(client): State<Client>,
+    Path(id): Path<ObjectId>,
+    Json(payload): Json<CreateQuote>,
+) -> StatusCode {
+    let now = chrono::Utc::now();
+    // TODO: remove repeated collection and database call
+    let collection = client.database("vinod").collection::<Quote>("quotes");
+    let filter = doc! { "_id": id };
+    let update = doc! { "$set": { "book": payload.book, "quote": payload.quote } };
+    let res = collection
+        .update_one(filter, update, None)
+        .await
+        .expect("Failed to update");
+
+    if let Some(updated_count) = Some(res.modified_count) {
+        if updated_count > 0 {
+            StatusCode::OK
+        } else {
+            StatusCode::NOT_FOUND
+        }
+    } else {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
+
+// pub async fn delete_quote(State(client): State<Client>, Path(id): Path<uuid::Uuid>) -> StatusCode {
+//
+//     match res {
+//         Ok(status) => status,
+//         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+//     }
+// }
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+mod handlers;
+mod utils;
+use axum::{
+    http::{header, HeaderValue},
+    routing::{delete, get, post, put},
+    Router,
+};
+use dotenv::dotenv;
+use mongodb::{options::ClientOptions, Client};
+use std::{env, time::Duration};
+use tower_http::{
+    limit::RequestBodyLimitLayer, set_header::SetResponseHeaderLayer, timeout::TimeoutLayer,
+    trace::TraceLayer,
+};
+use utils::connection::DatabaseConfig;
+
+#[tokio::main]
+async fn main() -> Result<(), axum::Error> {
+    dotenv().ok();
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
+    let database_config = DatabaseConfig::new();
+    let mut client_options = ClientOptions::parse(database_config.uri).await.unwrap();
+    client_options.connect_timeout = database_config.connection_timeout;
+    client_options.max_pool_size = database_config.max_pool_size;
+    client_options.min_pool_size = database_config.min_pool_size;
+    // the server will select the algorithm it supports from the list provided by the driver
+    client_options.compressors = database_config.compressors;
+    let client = Client::with_options(client_options).unwrap();
+
+    let app = Router::new()
+        .route("/", get(handlers::health))
+        .route("/quotes", post(handlers::create_quote))
+        .route("/quotes", get(handlers::read_quotes))
+        .route("/quotes/:id", put(handlers::update_quote))
+        // .route("/quotes/:id", delete(handlers::delete_quote))
+        // timeout requests after 10 secs, returning 408 status code
+        .layer(TimeoutLayer::new(Duration::from_secs(10)))
+        // don't allow request bodies larger than 1024 bytes, returning 413 status code
+        .layer(RequestBodyLimitLayer::new(1024))
+        .layer(TraceLayer::new_for_http())
+        .layer(SetResponseHeaderLayer::if_not_present(
+            header::SERVER,
+            HeaderValue::from_static("rust-axum"),
+        ));
+    let app = app.fallback(handlers::handler_404).with_state(client);
+
+    axum::Server::bind(&addr.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    println!("🚀 Server started successfully");
+    Ok(())
+}
